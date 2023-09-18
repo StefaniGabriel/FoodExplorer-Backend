0613d3e2a5afd5202e8596caec4ba1358559f50e
Stefani <stefanipereira118@gmail.com>
2023-09-18 10:44:37 -0300

    Atualização orders

--- src/database/knex/migrations/20230914131557_order.js
+++ src/database/knex/migrations/20230914131557_order.js
@@ -2,6 +2,7 @@ exports.up = knex => knex.schema.createTable("orders", table=> {
     table.increments("id");
     table.string("product_id");
     table.string("user_id");
+    table.string("quantity");
     table.foreign("product_id").references("product.id");
     table.foreign("user_id").references("users.id");
 }
